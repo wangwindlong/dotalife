89636340c0fd498bd493a29ddf1f69514ed14c33
wangwindlong <yunlong782@126.com>
2016-06-23 17:59:09 +0800

    add fabric code，use yunlong782@gmail.com 6629782

--- app/src/main/java/xyz/abcapp/dotalife/ui/LaunchActivity.java
+++ app/src/main/java/xyz/abcapp/dotalife/ui/LaunchActivity.java
@@ -3,6 +3,8 @@ package xyz.abcapp.dotalife.ui;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 /**
  * Created by wangyl on 16-5-30.
@@ -11,9 +13,22 @@ public class LaunchActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
+        // TODO: Move this to where you establish a user session
+        logUser();
+
         Intent intent = new Intent(this, MainActivity.class);
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(intent);
         finish();
     }
+
+    private void logUser() {
+        // TODO: Use the current user's information
+        // You can call any combination of these three methods
+        Crashlytics.setUserIdentifier("12345");
+        Crashlytics.setUserEmail("user@fabric.io");
+        Crashlytics.setUserName("Test User");
+    }
+
 }
